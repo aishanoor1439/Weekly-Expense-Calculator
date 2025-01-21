e2e696fd3e807f88e80d90b696ea1aa262b90a04
aishanoor1439 <aisha.noor1439@gmail.com>
2025-01-21 09:50:03 +0100

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weekly_Expense_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Array for storing expenses
+                double[] expenses = new double[7];
+                //Array for displaying days
+                string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+                double totalExpense = 0;
+
+                Console.WriteLine("Enter the refreshment and entertainment expenses for each day of the week.");
+
+                for (int i = 0; i < days.Length; i++)
+                {
+                    Console.WriteLine($"Enter expense for {days[i]}:");
+                    //Input: Expense for each day
+                    expenses[i] = double.Parse(Console.ReadLine());
+                    //Process: Adding all the expenses up
+                    totalExpense += expenses[i];
+                }
+
+                //Output: Displaying Output
+                Console.WriteLine($"The total weekly refreshment entertainment expense is: PKR {totalExpense}");
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
